1694c3129f8f76e419a7de3f4e3161f05c601153
floprsk <107794955+floprsk@users.noreply.github.com>
2023-09-01 15:20:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+function heatUpCoffe() {
+    const vapour = document.querySelector(".vapour");
+    const top = document.querySelector(".top");
+    vapour.classList.toggle("heated");
+}
+
+
